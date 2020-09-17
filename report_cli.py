885364ea324982ed8a987b11971b305bd61a8eba
kgu <kgu@thousandeyes.com>
2020-09-17 16:34:28 +0100

    Add remove filter, support different account group

--- report_cli.py
+++ report_cli.py
@@ -86,6 +86,25 @@ class ReportParser:
                 w['filters'][filter_name] = new_filter_values
         return report
 
+    def remove_filter(self, raw_report: dict, filter_code: str) -> dict:
+        if filter_code not in self.filter_code_to_name_map:
+            print(F'The filter code {filter_code} is not supported\n')
+            return raw_report
+
+        filter_name = self.filter_code_to_name_map[filter_code]
+
+        report = copy.deepcopy(raw_report)
+        for w in report['widgets']:
+            if 'numberCards' in w:
+                for c in w['numberCards']:
+                    if filter_name in c['filters']:
+                        del c['filters'][filter_name]
+            elif 'filters' in w:
+                if filter_name in w['filters']:
+                    del w['filters'][filter_name]
+
+        return report
+
 
 class ReportCli(Cmd):
     intro = 'You are authorised! Type ? for help :P\nIMPORTANT: Check the documentation of each command before use!!\n'
@@ -124,30 +143,44 @@ class ReportCli(Cmd):
     def do_get_report(self, arg):
         """
         Get raw report with ID and save it to a file
-        Usage: get ID DESTINATION_FILE
+        Usage: get ID DESTINATION_FILE [ACCOUNT_GROUP_ID]
+
+        ACCOUNT_GROUP_ID is optional. If not specified, try to fetch report from default account group
         """
         args = parse_arg(arg)
-        if len(args) is not 2:
+        if len(args) is 2:
+            self.session.params = {}
+        elif len(args) is 3:
+            self.session.params = {'aid': args[2]}
+        else:
             print(self.INVALID_ARGUMENTS_MSG)
             return False
 
         print('Getting your report')
         report = self.get_report(args[0])
-        if json is None:
+        if report is None:
             return False
 
         with open(args[1], 'w') as f:
             f.write(json.dumps(report, indent=4))
             print('Your report is saved!\n')
+
+        self.session.params = {}
         return False
 
     def do_get_template(self, arg):
         """
         Get report with filters cleared and save the template to a file
-        Usage: get_template ID DESTINATION_FILE
+        Usage: get_template ID DESTINATION_FILE [ACCOUNT_GROUP_ID]
+
+        ACCOUNT_GROUP_ID is optional. If not specified, try to fetch report from default account group
         """
         args = parse_arg(arg)
-        if len(args) is not 2:
+        if len(args) is 2:
+            self.session.params = {}
+        elif len(args) is 3:
+            self.session.params = {'aid': args[2]}
+        else:
             print(self.INVALID_ARGUMENTS_MSG)
             return False
 
@@ -159,6 +192,8 @@ class ReportCli(Cmd):
         with open(args[1], 'w') as f:
             f.write(json.dumps(ReportParser().get_template_report(report), indent=4))
             print('Your template report is saved!\n')
+
+        self.session.params = {}
         return False
 
     def do_get_endpoint_data(self, arg):
@@ -187,7 +222,8 @@ class ReportCli(Cmd):
         }
 
         data_type_code, destination_file_name = args
-        if data_type_code.lower() not in data_type_to_api_url_map:
+        data_type_code = data_type_code.lower()
+        if data_type_code not in data_type_to_api_url_map:
             print(F'Unsupported data type code {data_type_code}')
 
         api_url = data_type_to_api_url_map[data_type_code]
@@ -204,7 +240,9 @@ class ReportCli(Cmd):
     def do_add_filter(self, arg):
         """
         Add a new filter to all the widgets
-        Usage: add_filter ID FILTER_NAME FILTER_VALUES_FILE
+        Usage: add_filter ID FILTER_NAME FILTER_VALUES_FILE [ACCOUNT_GROUP_ID]
+
+        ACCOUNT_GROUP_ID is optional. If not specified, try to add filter to the report from default account group
 
         FILTER_NAME list:
         EA  -- Endpoint Agents
@@ -217,20 +255,65 @@ class ReportCli(Cmd):
         Note: when changing filter of Endpoint Agents use agent ID!!
         """
         args = parse_arg(arg)
-        if len(args) is not 3:
+        if len(args) is 3:
+            self.session.params = {}
+        elif len(args) is 4:
+            self.session.params = {'aid': args[3]}
+        else:
             print(self.INVALID_ARGUMENTS_MSG)
             return False
 
-        return self.update_report_filter(args, ReportParser().add_filter_value)
+        return self.update_report_filter(args[:3], ReportParser().add_filter_value)
+
+    def do_remove_filter(self, arg):
+        """
+        Remove the filter in all widget
+        Usage: remove_filter REPORT_ID FILTER_NAME [ACCOUNT_GROUP_ID]
+
+        ACCOUNT_GROUP_ID is optional. If not specified, try to remove the filter in report from default account group
+
+
+        FILTER_NAME list:
+        EA  -- Endpoint Agents
+        EAL -- Endpoint Agent Labels
+        LOC -- Location
+        PN  -- Private Network
+        NET -- Network
+        MN  -- Monitored Network
+        """
+        args = parse_arg(arg)
+        if len(args) is 2:
+            self.session.params = {}
+        elif len(args) is 3:
+            self.session.params = {'aid': args[2]}
+        else:
+            print(self.INVALID_ARGUMENTS_MSG)
+            return False
+
+        report_id, filter_code = args[:2]
+
+        raw_report = self.get_report(report_id)
+        if raw_report is None:
+            return False
+
+        new_report = ReportParser().remove_filter(raw_report, filter_code.lower())
+        if new_report != raw_report:
+            self.session.post(F'{self.REPORT_API_BASE}/{report_id}/update', json=new_report)
+            print('Done!\n')
+
+        self.session.params = {}
+        return False
 
     def do_change_filter_value(self, arg):
         """
         Change the filter's value in the report, the new filter values is specified in a file where the values are
         separated with a newline.
-        Usage: change_filter_value ID FILTER_NAME FILTER_VALUES_FILE
+        Usage: change_filter_value ID FILTER_NAME FILTER_VALUES_FILE [ACCOUNT_GROUP_ID]
+
+        ACCOUNT_GROUP_ID is optional. If not specified, try to remove the filter in report from default account group
 
 
-        filter_name list:
+        FILTER_NAME list:
         EA  -- Endpoint Agents
         EAL -- Endpoint Agent Labels
         LOC -- Location
@@ -241,41 +324,49 @@ class ReportCli(Cmd):
         Note: when changing filter of Endpoint Agents use agent ID!!
         """
         args = parse_arg(arg)
-        if len(args) is not 3:
+        if len(args) is 3:
+            self.session.params = {}
+        elif len(args) is 4:
+            self.session.params = {'aid': args[3]}
+        else:
             print(self.INVALID_ARGUMENTS_MSG)
             return False
 
-        return self.update_report_filter(args, ReportParser().change_filter_value)
+        return self.update_report_filter(args[:3], ReportParser().change_filter_value)
 
     def do_export_to_acc_group(self, arg):
         """
         Export report from one account group to another account group
-        Usage: export_to_acc_group SOURCE_REPORT_ID DESTINATION_ACC_GROUP_ID [USERNAME AUTH_TOKEN]
+        Usage: export_to_acc_group SOURCE_REPORT_ID SOURCE_ACC_GROUP_ID DESTINATION_ACC_GROUP_ID [USERNAME AUTH_TOKEN]
 
         Note: USERNAME and AUTH_TOKEN are optional if your current account is in the destination account group too
         """
         args = parse_arg(arg)
-        print(len(args))
-        if len(args) is not 2 and len(args) is not 4:
+        if len(args) is 3:
+            auth = self.session.auth
+        elif len(args) is 5:
+            auth = (args[3], args[4])
+        else:
             print(self.INVALID_ARGUMENTS_MSG)
             return False
 
-        report_id, acc_group_id = args[:2]
+        report_id, src_acc_group_id, dest_acc_group_id = args[:3]
 
+        self.session.params = {'aid': src_acc_group_id}
         report = self.get_report(report_id)
         if report is None:
             return False
 
-        auth = self.session.auth if len(args) is 2 else (args[2], args[3])
-        r = requests.post(F'{self.REPORT_API_BASE}/create', params={'aid': acc_group_id}, auth=auth, json=report)
+        r = requests.post(F'{self.REPORT_API_BASE}/create', params={'aid': dest_acc_group_id}, auth=auth, json=report)
 
         if r.status_code is 401:
             print('You are not authorised, check your credential for the destination account group.\n')
         elif r is None or r.text is None:
             print('Something went wrong :/\n')
         else:
-            print(F'Your report is successfully exported to account group {acc_group_id}!\n')
+            print(F'Your report is successfully exported to account group {dest_acc_group_id}!\n')
 
+        self.session.params = {}
         return False
 
     def get_report(self, report_id: str):
@@ -297,10 +388,11 @@ class ReportCli(Cmd):
             values = list(map(lambda x: x.strip(), f.read().splitlines()))
 
         new_report = report_parser_func(raw_report, filter_code.lower(), values)
-        with open('lmao.json', 'w') as f:
-            f.write(json.dumps(new_report, indent=4))
-        # self.session.post(F'{self.REPORT_API_BASE}/{report_id}/update', json=new_report)
-        print('Done!\n')
+        if new_report != raw_report:
+            self.session.post(F'{self.REPORT_API_BASE}/{report_id}/update', json=new_report)
+            print('Done!\n')
+
+        self.session.params = {}
         return False
 
 
